dc97bb938c2fd85c7292646789d084f8fc302155
doccomsa <newcomsa@nate.com>
2025-02-19 15:51:39 +0900

    deploy 11

--- src/main/java/com/ezen/shop/admin/product/AdProductController.java
+++ src/main/java/com/ezen/shop/admin/product/AdProductController.java
@@ -1,5 +1,6 @@
 package com.ezen.shop.admin.product;
 
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
 import java.io.PrintWriter;
@@ -100,6 +101,7 @@ public class AdProductController {
 			
 			// C:\\Dev\\upload\\ckeditor\\abc.gif
 			String ckUploadPath = uploadCKPath + "\\" + fileName;
+			ckUploadPath.replace("\\", File.separator);
 			
 			//스트림 out객체생성이 되면, 해당 경로에 파일은 생성된다. 파일크기는 0byte
 			out = new FileOutputStream(ckUploadPath);
